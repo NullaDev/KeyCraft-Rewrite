4ee155426c091044f0ba52ee1ab8d155b711610c
Kanbe-Kotori <1438726022@qq.com>
2016-02-17 11:38:50 +0800

    增加食物基类

--- main/java/org/nulla/kcrw/KeyCraft_Rewrite.java
+++ main/java/org/nulla/kcrw/KeyCraft_Rewrite.java
@@ -11,7 +11,7 @@ public class KeyCraft_Rewrite {
 	
 	public static final String MODID = "kcrw";
 	public static final String MODNAME = "KeyCraft Rewrite Ver.";
-    public static final String VERSION = "Demo";
+    public static final String VERSION = "Demo20160217";
     
     @SidedProxy(clientSide = "org.nulla.kcrw.KCClientProxy",
             	serverSide = "org.nulla.kcrw.KCCommonProxy")
---
+++ main/java/org/nulla/kcrw/item/ItemFoodKC.java
@@ -0,0 +1,44 @@
+package org.nulla.kcrw.item;
+
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class ItemFoodKC extends ItemFood {
+	
+	public interface ICallback {
+		void onFoodEaten(ItemStack stack, World world, EntityPlayer player);
+		void addInformation(ItemStack stack, EntityPlayer player, List information, boolean p_77624_4_);
+	}
+	
+	protected ICallback Callback = null;
+
+	public ItemFoodKC(int amount) {
+		super(amount, 0.6F, false);	//后两个是干嘛的
+	}
+	
+	public ItemFoodKC setCallback(ICallback callback) {
+		Callback = callback;
+		return this;
+	}
+	
+	@Override
+	protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player) {
+		super.onFoodEaten(stack, world, player);
+		if (Callback != null) {
+			Callback.onFoodEaten(stack, world, player);
+		}
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List information, boolean p_77624_4_) {
+		super.addInformation(stack, player, information, p_77624_4_);
+		if (Callback != null) {
+			Callback.addInformation(stack, player, information, p_77624_4_);
+		}
+	}
+
+}
