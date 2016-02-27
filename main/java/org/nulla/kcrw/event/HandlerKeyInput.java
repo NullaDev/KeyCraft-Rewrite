afeb92c830a7648cdacb2a6cf27889b2abd8cb16
noname00 <xfgryujk@126.com>
2016-02-27 23:29:25 +0800

    成功播放pump♂it

--- main/java/org/nulla/kcrw/event/HandlerKeyInput.java
+++ main/java/org/nulla/kcrw/event/HandlerKeyInput.java
@@ -2,12 +2,16 @@ package org.nulla.kcrw.event;
 
 import org.nulla.kcrw.KCClientProxy;
 import org.nulla.kcrw.KCUtils;
+import org.nulla.kcrw.KeyCraft_Rewrite;
 import org.nulla.kcrw.skill.Skill;
 import org.nulla.kcrw.skill.SkillUtils;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent.KeyInputEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 
 public class HandlerKeyInput {
 	
@@ -22,6 +26,11 @@ public class HandlerKeyInput {
 			} else {
 				skill = SkillUtils.getSkillInSlot(player, 0);
 				System.out.println("使用技能0");
+
+				// 播放音乐测试
+				Minecraft.getMinecraft().getSoundHandler().playSound(PositionedSoundRecord.func_147673_a(
+						new ResourceLocation(KeyCraft_Rewrite.MODID, "music.test") // 见 resources\assets\kcrw\sounds.json
+						));
 			}
 		} else if (KCClientProxy.kbSkill2.isPressed()) {
 		    if (KCUtils.isShiftKeyDown()) {
