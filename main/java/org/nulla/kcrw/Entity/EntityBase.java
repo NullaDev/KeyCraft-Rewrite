7d0b2dec06e5d73b82d56923f6e6bc2d67e69d5e
SalimTerryLi <lhf2613@gmail.com>
2016-03-10 21:57:40 +0800

    开坑Entity，先撇个包作为flag...

---
+++ main/java/org/nulla/kcrw/Entity/EntityBase.java
@@ -0,0 +1,32 @@
+package org.nulla.kcrw.Entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public class EntityBase extends Entity{
+
+	public EntityBase(World par1)
+	{
+		super(par1);
+	}
+
+	@Override
+	protected void entityInit()
+{
+		
+	}
+
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound par1)
+{
+		
+	}
+
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound par1)
+{
+		
+	}
+
+}
