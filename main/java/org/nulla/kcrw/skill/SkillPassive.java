262df85c04d0a89f234bd8c52aa794bcc1cd9f13
Kanbe-Kotori <1438726022@qq.com>
2016-03-26 18:07:17 +0800

    完善被动技能

--- main/java/org/nulla/kcrw/skill/SkillPassive.java
+++ main/java/org/nulla/kcrw/skill/SkillPassive.java
@@ -1,6 +1,7 @@
 package org.nulla.kcrw.skill;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 
 public class SkillPassive extends Skill {
 
@@ -18,6 +19,42 @@ public class SkillPassive extends Skill {
 		return true;
 	}
 	
+	public boolean trigger(EntityPlayer player) {
+		// 检查拥有技能
+		if (!hasSkill(player))
+			return false;
+		// 检查CD
+		if (!checkCD(player))
+			return false;
+		// 检查被动
+		if (!isPassive()) {
+			return false;
+		}
+		// 检查欧若拉点
+		if (SkillUtils.getAuroraPoint(player) <= mAuroraCost) // 不让欧若拉变为0
+			return false;
+				
+		// 客户端发使用技能包
+		if (player.worldObj.isRemote)
+			SkillNetwork.Channel.sendToServer(SkillNetwork.createUseSkillPacket(mID));
+		boolean res = onTrigger(player);
+				
+		if (res) {
+			// 设置上次使用时间
+			setLastUseTime(player, player.worldObj.getTotalWorldTime());
+			// 减欧若拉点
+			SkillUtils.modifyAuroraPoint(player, -mAuroraCost);
+		}
+				
+		// 服务端发同步技能包
+		if (player instanceof EntityPlayerMP)
+			SkillNetwork.Channel.sendTo(SkillNetwork.createSyncSkillPacket(player), (EntityPlayerMP)player);
+		
+		return res;
+		
+	}
+	
+	/** 重载实现使用技能 */
 	public boolean onTrigger(EntityPlayer player) {
 		return false;
 	}
