8891aedd286b7a02224bd16de1cf27fce2951e5c
SalimTerryLi <lhf2613@gmail.com>
2016-07-18 22:19:07 +0800

    修了下狗狗们尾巴鬼畜的问题...

--- main/java/org/nulla/kcrw/entity/entityliving/RendererBlackDog.java
+++ main/java/org/nulla/kcrw/entity/entityliving/RendererBlackDog.java
@@ -6,6 +6,8 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.util.ResourceLocation;
 
 public class RendererBlackDog extends RenderLiving {
@@ -18,5 +20,10 @@ public class RendererBlackDog extends RenderLiving {
 	protected ResourceLocation getEntityTexture(Entity p_110775_1_) {
 		return new ResourceLocation(KeyCraft_Rewrite.MODID, "textures/models/entities/black_dog.png");
 	}
+	
+	protected float handleRotationFloat(EntityLivingBase par1, float par2)
+    {
+        return 90;
+    }
 
 }
